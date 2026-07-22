28a82abcc428160c62d08139951f1bee85abe3cc
usman061ng <u061ng@gmail.com>
2026-07-22 10:06:55 +0500

    added new buttons

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const productCards = document.querySelectorAll('.product-item');
-const greenColorHash = '#15e464';
-const blueColorHash = '#e9da11';
-
-productCards.forEach((card) => {
-  card.style.backgroundColor = greenColorHash;
-});
-
 const firstProductCard = document.querySelector('.product-item');
-if (firstProductCard) {
-  firstProductCard.style.backgroundColor = blueColorHash;
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
+const blueColorHash = '#00ffea';
+
+if (changeColorFirstCardButton && firstProductCard) {
+  changeColorFirstCardButton.addEventListener('click', () => {
+    firstProductCard.style.backgroundColor = blueColorHash;
+  });
 }
 
-if (firstProductCard) {
-  firstProductCard.addEventListener('click', openGoogle);
+const productCards = document.querySelectorAll('.product-item');
+const changeColorAllCardButton = document.querySelector('#change-color-all-card');
+const greenColorHash = '#0bee29';
+
+if (changeColorAllCardButton) {
+  changeColorAllCardButton.addEventListener('click', () => {
+    productCards.forEach((card) => {
+      card.style.backgroundColor = greenColorHash;
+    });
+  });
+}
+
+const openGoogleButton = document.querySelector('#open-google');
+
+if (openGoogleButton) {
+  openGoogleButton.addEventListener('click', openGoogle);
 }
 
 function openGoogle() {
@@ -20,14 +31,13 @@ function openGoogle() {
 
   if (answer === true) {
     window.open('https://google.com', '_blank');
-  } else {
-    return;
   }
 }
 
-const secondProductCard = productCards[1];
-if (secondProductCard) {
-  secondProductCard.addEventListener('click', () => outputConsoleLog('ДЗ №6'));
+const outputLogButton = document.querySelector('#output-console-log');
+
+if (outputLogButton) {
+  outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №6'));
 }
 
 function outputConsoleLog(message) {
@@ -36,6 +46,7 @@ function outputConsoleLog(message) {
 }
 
 const title = document.querySelector('.title');
+
 if (title) {
   title.addEventListener('mouseenter', () => {
     console.log(title.textContent);
@@ -43,6 +54,7 @@ if (title) {
 }
 
 const toggleButton = document.querySelector('#toggle-btn');
+
 if (toggleButton) {
   toggleButton.addEventListener('click', () => {
     toggleButton.classList.toggle('active');
